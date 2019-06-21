448c18642940c03d97d949868fab157bb1839633
franciscomartinez <turtle829@fullsail.edu>
2019-06-21 13:11:54 -0400

    Initial Commit

---
+++ Escape Artist Collective/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Escape Artist Collective
+//
+//  Created by FRANCISCO MARTINEZ on 6/21/19.
+//  Copyright © 2019 FRANCISCO MARTINEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
